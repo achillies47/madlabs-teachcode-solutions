8d1047a7fa1502eeaa5f2719e50f0dcdd9875e47
achillies47 <abhijithshajim@gmail.com>
2017-10-25 16:53:55 +0000

    13Task Submitted

---
+++ solution13test14.py
@@ -0,0 +1,16 @@
+numOne = 1 
+numOne -= 1 #Modify this statement 
+numTwo = 50 
+numTwo += 2 #Modify this statement 
+numThree = 40 
+numThree -= 2 #Modify this statement 
+numFour = 20 
+numFour *= 2 #Modify this statement 
+numFive = 80 
+numFive /= 4 #Modify this statement
+numSix = 100 
+numSix -= 100 #Modify this statement
+print(numOne, numTwo, numThree, numFour, numFive, numSix)
+
+
+#0 52 38 40 20.0 0
