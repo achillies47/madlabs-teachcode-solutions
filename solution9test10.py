52d95a33bffc61b0b6196a74d72397e55123db4e
achillies47 <abhijithshajim@gmail.com>
2017-10-25 14:35:31 +0000

    9Task Submitted

---
+++ solution9test10.py
@@ -0,0 +1,6 @@
+stringVariable = "Play a game with me if you're a Hacker" 
+print(stringVariable[-6]) 
+print(stringVariable[20 :]) #Prints the sliced string 
+print(stringVariable[ : 11]) #Prints the sliced string 
+print(stringVariable + " You Just Concatinated ! Hacker !") #Prints the joined string
+print(stringVariable * 3)
